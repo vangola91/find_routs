3b0a4ec9f56aca5c30928488369a028e12cff519
Aliksandrmax <88476601+vangola91@users.noreply.github.com>
2022-07-10 12:43:16 +0500

    newe

--- routes/forms.py
+++ routes/forms.py
@@ -1,27 +1,26 @@
 from cities.models import City
 from django import forms
 
-class RouteForm(forms.Form):
 
+class RouteForm(forms.Form):
     from_city = forms.ModelChoiceField(
         label='Откуда', queryset=City.objects.all(), widget=forms.Select(
-        attrs={'class': 'form-control'}
+            attrs={'class': 'form-control js-example-basic-single'}
         )
     )
 
     to_city = forms.ModelChoiceField(
         label='Куда', queryset=City.objects.all(), widget=forms.Select(
-        attrs={'class': 'form-control'}
+            attrs={'class': 'form-control js-example-basic-single'}
         )
     )
     cities = forms.ModelMultipleChoiceField(
         label='Через города', queryset=City.objects.all(),
         required=False, widget=forms.SelectMultiple(
-            attrs={'class': 'form-control'}
+            attrs={'class': 'form-control js-example-basic-multiple'}
         )
     )
     travelling_time = forms.IntegerField(
         label='Время в пути', widget=forms.NumberInput(attrs={
-            'class': 'form-control', 'placeholder': 'Время в пути'}
-        )
+            'class': 'form-control', 'placeholder': 'Время в пути'})
     )
